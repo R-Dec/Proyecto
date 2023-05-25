53ab430db0183be262d5cf187823fccf7f759fca
R-Dec <TheJoaca@outlook.com>
2023-05-25 15:05:30 -0500

    Formateo de texto

--- src/main/main/Fecha.java
+++ src/main/main/Fecha.java
@@ -5,30 +5,52 @@ public class Fecha {
 	int minuto;
 	int dia;
 	int mes;
-	int ano;
-	
-	public Fecha(int day,int month,int year)
-	{
-		dia = day; mes = month; ano = year;
+	int anual;
+
+	public Fecha(int day, int month, int year) {
+		dia = day;
+		mes = month;
+		anual = year;
 		hora = 0;
 		minuto = 0;
 	}
-	
-	public Fecha(int hour, int minute, int day,int month,int year)
-	{
-		dia = day; mes = month; ano = year;
-		hora = hour; minuto = minute;
+
+	public Fecha(int hour, int minute, int day, int month, int year) {
+		dia = day;
+		mes = month;
+		anual = year;
+		hora = hour;
+		minuto = minute;
 	}
-	
-	public void setFecha(int day, int month, int year){dia = day; mes = month; ano = year;}
-	
-	public void setHora(int hour, int minute){hora = hour; minuto = minute;}
-	
-	public int getAnno(){return ano;}
-	public int getMes(){return mes;}
-	public int getDia(){return dia;}
-	
-	public String getFecha(){return String.format("%d/%d/%d",dia,mes,ano);}
-	
-	public String getHora(){return String.format("%d:%d", hora, minuto);}
+
+	public void setFecha(int day, int month, int year) {
+		dia = day;
+		mes = month;
+		anual = year;
+	}
+
+	public void setHora(int hour, int minute) {
+		hora = hour;
+		minuto = minute;
+	}
+
+	public int getAnno() {
+		return anual;
+	}
+
+	public int getMes() {
+		return mes;
+	}
+
+	public int getDia() {
+		return dia;
+	}
+
+	public String getFecha() {
+		return String.format("%d/%d/%d", dia, mes, anual);
+	}
+
+	public String getHora() {
+		return String.format("%d:%d", hora, minuto);
+	}
 }
